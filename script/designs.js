19f9ed37f60aaa8d3a1cf3c9b50f4c6551d24a28
Rafael Martinez <rrhhmmtt@gmail.com>
2023-08-07 18:59:45 -0500

    primer test cors a back

--- script/designs.js
+++ script/designs.js
@@ -1,2 +1,8 @@
 console.log('aqui estoy designs');
 
+async function getDesigns() {
+    let response = await fetch('https://shopseekerback-production.up.railway.app/api/designs')
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+}
+getDesigns();
