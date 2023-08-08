b638229f35909b090b84b142c6b492764dcc7237
Rafael Martinez <rrhhmmtt@gmail.com>
2023-08-07 19:31:03 -0500

    se ajusta endpoint

--- script/designs.js
+++ script/designs.js
@@ -2,7 +2,7 @@ console.log('aqui estoy designs');
 
 async function getDesigns() {
     let response = await fetch('https://shopseekerback-production.up.railway.app/api/designs', {
-        method: "POST",
+        method: "GET",
         credentials: "include",
     })
         .then((res) => res.json())
