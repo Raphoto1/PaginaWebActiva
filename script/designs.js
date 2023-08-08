e1021faaa07e669e17b53ce8ed67a75200189191
Rafael Martinez <rrhhmmtt@gmail.com>
2023-08-07 19:27:33 -0500

    se hace test de cors

--- script/designs.js
+++ script/designs.js
@@ -1,7 +1,10 @@
 console.log('aqui estoy designs');
 
 async function getDesigns() {
-    let response = await fetch('https://shopseekerback-production.up.railway.app/api/designs')
+    let response = await fetch('https://shopseekerback-production.up.railway.app/api/designs', {
+        method: "POST",
+        credentials: "include",
+    })
         .then((res) => res.json())
         .then((data) => console.log(data));
 }
