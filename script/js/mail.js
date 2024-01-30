44b1ca0feb4aa448861393a059a98aba607e3293
Rafael Martinez <rrhhmmtt@gmail.com>
2024-01-30 11:33:56 -0500

    se actualizan algunos skills

--- script/js/mail.js
+++ script/js/mail.js
@@ -1,29 +1,30 @@
 const formDom = document.querySelector(`form`);
-const url = "https://shops.creativerafa.com/api/contact/"
+const url = "https://shops.creativerafa.com/api/contact/";
 formDom.addEventListener(`submit`, handleSubmit);
 
 async function handleSubmit(event) {
-    console.log('entro al handle');
+  console.log("entro al handle");
   event.preventDefault();
-    const form = new FormData(this);
-    const data = Object.fromEntries(form.entries()); // Convertir el formulario a un objeto
-const json = JSON.stringify(data); // Convertir el objeto a una cadena JSON
+    const form = new FormData(formDom);
+
+    const formTest = new FormData();
+    formTest.append('name', 'rafa')
+    formTest.append('email', 'rafa@rafa.com')
+    formTest.append('message', 'mensaje test')
   const response = await fetch(url, {
-      method: 'POST',
-      body: form,
-      headers: {
-        "Content-Type": "application/json",
-      },
+    method: "POST",
+      body: formTest,
+
   })
     .then((res) => console.log(res))
     .then((data) => {
-        alert("Gracias!!!!");
-        window.location.replace("https://www.creativerafa.com");
+      alert("Gracias!!!!");
+      window.location.replace("https://www.creativerafa.com");
       console.log(data);
     })
-      .catch(error => {
-          alert("Ocurrió un error: " + error.message)
-          console.log(error);
-    })
+    .catch((error) => {
+      alert("Ocurrió un error: " + error.message);
+      console.log(error);
+    });
   console.log(response);
 }
